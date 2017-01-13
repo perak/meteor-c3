086022cb46338689f7e80cd1143ba55fba87113a
Petar Korponaić <petar.korponaic@gmail.com>
2017-01-13 16:36:58 +0100

    Oops, version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'perak:c3',
 	summary: "Reactive C3 charting library based on D3",
-	version: "1.0.6",
+	version: "1.0.7",
 	git: "https://github.com/perak/meteor-c3.git"
 });
 
