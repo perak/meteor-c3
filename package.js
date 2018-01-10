66787a84e2e917dc38ce44acc40fa493d8afec37
Petar Korponaić <petar.korponaic@gmail.com>
2018-01-10 14:12:46 +0100

    removed c3 submodule

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'perak:c3',
 	summary: "Reactive C3 charting library based on D3",
-	version: "1.0.9",
+	version: "1.1.0",
 	git: "https://github.com/perak/meteor-c3.git"
 });
 
