c0f4bc33cad4d9e29a3280745625ead8b6309c2f
Petar Korponaić <petar.korponaic@gmail.com>
2018-01-10 13:58:31 +0100

    Fixed problem with submodule

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'perak:c3',
 	summary: "Reactive C3 charting library based on D3",
-	version: "1.0.8",
+	version: "1.0.9",
 	git: "https://github.com/perak/meteor-c3.git"
 });
 
